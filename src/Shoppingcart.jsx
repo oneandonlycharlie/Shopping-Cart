6b7bec14bac15d92f95b60a8d5227f0321aa2834
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2024-11-11 23:26:30 +0800

    Build: add shopping cart to display selcted items

---
+++ src/Shoppingcart.jsx
@@ -0,0 +1,34 @@
+import { Link,useOutletContext } from "react-router-dom";
+
+function ShoppingCart(){
+
+ const [inventory,setInventory] = useOutletContext();
+
+    const selectedItems = inventory.filter((item) => item.selected == true )
+
+    let subTotal = 0;
+
+    return (
+        <>
+        <p>Welcome to the check out page</p>
+        <div>
+            {selectedItems.map(item => {
+                subTotal += item.price * item.quantity
+                return (
+                    <div key={item.id}>
+                        <p>{item.title}</p>
+                        <p>{item.price}</p>
+                        <p>{item.quantity}</p>
+                    </div>
+                )
+            })}
+        </div>
+        <p>{subTotal}</p>
+        <Link to="/shoplist">Back to shopping</Link>
+        <button>Check out</button>
+        </>
+    )
+
+}
+
+export {ShoppingCart}
