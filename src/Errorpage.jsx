1bb3c9736f524b4f679a046ace8af58df6043ab9
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2024-11-11 20:45:58 +0800

    Build: set up error page

---
+++ src/Errorpage.jsx
@@ -0,0 +1,13 @@
+
+
+export default function ErrorPage (){
+    
+
+    return (
+        <div id="error-page">
+            <h1>Oops</h1>
+            <p>An expected error has occurred</p>
+        </div>
+        
+    )
+}
